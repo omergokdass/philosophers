785810c3692bc155ad84e6b9125fbf2abce4e202
Omer Faruk Gokdas <ogokdas@k1m06s01.42istanbul.com.tr>
2026-01-10 00:01:57 +0300

    add mutex(forks) and philo-info entegration

--- philo.c
+++ philo.c
@@ -1,41 +1,75 @@
 #include "philo.h"
-
 void *to_do(void *asd)
 {
-
+    t_philo *inf = (t_philo *)asd;
+    while (1)
+    {
+        if (inf->id % 2 == 0)
+        {
+            pthread_mutex_lock(inf->l_fork);
+            printf("llu %d has taken a fork\n", inf->id);
+            pthread_mutex_lock(inf->r_fork);
+            printf("llu %d has taken a fork\n", inf->id);
+        }
+        else
+        {
+            pthread_mutex_lock(inf->r_fork);
+            printf("llu %d has taken a fork\n", inf->id);
+            pthread_mutex_lock(inf->l_fork);
+            printf("llu %d has taken a fork\n", inf->id);
+        }
+        printf("llu %d is eating", inf->id);
+        pthread_mutex_unlock(inf->l_fork);
+        pthread_mutex_unlock(inf->r_fork);
+        printf("llu %d is sleeping", inf->id);
+        usleep(inf->info->time_sleep * 1000);
+        printf("llu %d is thinking", inf->id);
+    }
+    return NULL;
 }
+
 void create_thread(t_info *asd)
 {
     int i = 0;
-    while(i<asd->number_philos)
-    {   
-        
-        pthread_create(asd->philos[i],NULL, to_do, asd->filo);
+    asd->filo = malloc(sizeof(t_philo) * asd->number_philos);
+    asd->philos = malloc(sizeof(pthread_t) * asd->number_philos);
+    while (i < asd->number_philos)
+    {
+        asd->filo[i].id = i + 1;
+        asd->filo[i].l_fork = &asd->forks[i];
+        asd->filo[i].r_fork = &asd->forks[(i + 1) % asd->number_philos];
+        asd->filo[i].info = asd;
+        pthread_create(&asd->philos[i], NULL, to_do, &asd->filo[i]);
         i++;
     }
-
 }
 void init(t_info *inf, char **av)
 {
-    
+
     inf->number_philos = ft_atoi(av[1]);
     inf->die_time = ft_atoi(av[2]);
     inf->eating_time = ft_atoi(av[3]);
     inf->time_sleep = ft_atoi(av[4]);
-
+    inf->forks = malloc(sizeof(pthread_mutex_t) * inf->number_philos);
     int i = 0;
-    while(i < inf->number_philos)
-    {
-        // i will create fork 
-        i++;
-    }
 
+    while (i < inf->number_philos)
+    {
+        pthread_mutex_init(&inf->forks[i], NULL);
+        i++; 
+    }
 }
+
 int main(int ac, char **av)
 {
-    t_info *inf = malloc(sizeof(t_info*));
-    init(inf,av);
+    t_info *inf = malloc(sizeof(t_info ));
+    init(inf, av);
+    create_thread(inf);
 
     int i = 0;
-
+    while (i < inf->number_philos)
+    {
+        pthread_join(inf->philos[i], NULL);
+        i++;
+    }
 }
--- philo.h
+++ philo.h
@@ -2,7 +2,19 @@
 # define PHILO_H
 
 # include <stdlib.h>
+# include <unistd.h>
 # include <pthread.h>
+# include <stdio.h>
+
+typedef struct s_philo
+{
+    int id;
+    int last_meal_time;
+    pthread_mutex_t *l_fork;
+    pthread_mutex_t *r_fork;
+    struct s_info *info;
+
+}t_philo ;
 
 typedef struct s_info
 {
@@ -10,25 +22,15 @@ typedef struct s_info
     int eating_time;
     int die_time;
     int time_sleep;
+    int must_eat;
+    int eat_count;
     int is_dead;
     pthread_t *philos;
     pthread_mutex_t *forks;
     pthread_mutex_t *lock_write;
-    struct s_philo *filo;
-    
+    t_philo *filo;
 } t_info;
 
-typedef struct s_philo
-{
-    int id;
-    int eat_count;
-    int last_meal_time;
-    pthread_mutex_t *l_fork;
-    pthread_mutex_t *r_fork;
-    struct s_info *info;
-
-    
-}t_philo ;
 
 
 int	ft_atoi(const char *nptr);
